04f9471a3095d1d1b888f6319ab6b930945fd184
Michael Nedokushev <grouzen.hexy@gmail.com>
2025-05-22 23:44:10 +0100

    Add detect_mode

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
 use std::io;
 
-use ollana::{client_proxy::ClientProxy, server_proxy::ServerProxy};
+use log::info;
+use ollana::{client_proxy::ClientProxy, ollama::Ollama, server_proxy::ServerProxy};
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
+    env_logger::init();
+
+    info!("Starting Ollana...");
     // To run the proxy we need:
     // 1. check if there is no ollama running on localhost:port where port is a default or configured ollama port
     //    a. in such case, start ServerProxy
     //    b. otherwise, start ClientProxy
     // let client_proxy = ClientProxy::default();
-    let server_proxy = ServerProxy::default();
+    let ollama = Ollama::default();
 
-    // client_proxy.run_server().await
-    server_proxy.run_server().await
+    match detect_mode(ollama).await {
+        Mode::Server => {
+            info!("Running in Server Mode");
+            ServerProxy::default().run_server().await
+        }
+        Mode::Client => {
+            info!("Running in Client Mode");
+            ClientProxy::default().run_server().await
+        }
+    }
 }
 
 enum Mode {
     Client,
     Server,
+}
+
+async fn detect_mode(ollama: Ollama) -> Mode {
+    match ollama.get_version().await {
+        Ok(_) => Mode::Server,
+        Err(_) => Mode::Client,
+    }
 }
